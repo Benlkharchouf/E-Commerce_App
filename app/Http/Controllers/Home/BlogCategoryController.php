ce43244f60536d096ef696ae8e50f4b7eb2e48a9
unknown <hibaChaymae70@gmail.com>
2022-07-10 02:57:58 +0100

    add 2022_03_20_073209_create_blogs_table.php in migrations

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -37,22 +37,24 @@ class BlogCategoryController extends Controller
         $blogcategory = BlogCategory::findOrFail($id);
         return view('admin.blog_category.blog_category_edit',compact('blogcategory'));
 
-
     } // End Method
 
 
     public function UpdateBlogCategory(Request $request,$id){
 
-        BlogCategory::findOrFail($id)->update([
-               'blog_category' => $request->blog_category,
-           ]);
+         BlogCategory::findOrFail($id)->update([
+                'blog_category' => $request->blog_category,
 
-           $notification = array(
-           'message' => 'Blog Category Updated Successfully',
-           'alert-type' => 'success'
-       );
-       return redirect()->route('all.blog.category')->with($notification);
-   } // End Method
+            ]);
+
+            $notification = array(
+            'message' => 'Blog Category Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.blog.category')->with($notification);
+
+    } // End Method
 
     public function DeleteBlogCategory($id){
 
--- database/migrations/2022_03_20_073209_create_blogs_table.php
+++ database/migrations/2022_03_20_073209_create_blogs_table.php
@@ -14,13 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
+
             $table->id();
             $table->string('blog_category_id')->nullable();
             $table->string('blog_title')->nullable();
             $table->string('blog_image')->nullable();
             $table->string('blog_tags')->nullable();
-            $table->text('blog_description')->nullable(); 
+            $table->text('blog_description')->nullable();
             $table->timestamps();
+
         });
     }
 
